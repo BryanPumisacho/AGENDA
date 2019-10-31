552bad3f45c4b9af366845f24190aeb20ac0767e
Fundamentos DS <Fundamentos DS@1-21-01-MD-25>
2019-10-31 09:31:39 -0500

    Inicio Agenda

---
+++ Agenda/src/agenda/Agenda.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Fundamentos DS
+ */
+public class Agenda {
+
+    /**
+     * @param args the command line arguments
+     */
+      String nombre;
+      int numero;
+      String direccion;
+      
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+     JOptionPane.showInputDialog("Ingresa el nombre para registrarlo en tu agenda");
+     
+     
+    
+      
+    }
+    
+}
